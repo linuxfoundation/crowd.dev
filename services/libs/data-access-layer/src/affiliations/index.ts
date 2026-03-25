d4cc31c58a6b686bc5d26ab8bb241c1f2c23d36e
Umberto Sgueglia <usgueglia@contractor.linuxfoundation.org>
2026-03-25 16:55:23 +0100

    feat: use join instead of in
    Signed-off-by: Umberto Sgueglia <usgueglia@contractor.linuxfoundation.org>

--- services/libs/data-access-layer/src/affiliations/index.ts
+++ services/libs/data-access-layer/src/affiliations/index.ts
@@ -40,11 +40,13 @@ export async function findWorkExperiencesBulk(
           AND "deletedAt" IS NULL
       ),
       aggs AS (
-        SELECT "organizationId", COUNT(DISTINCT "memberId") AS total_count
-        FROM "memberOrganizations"
-        WHERE "organizationId" IN (SELECT "organizationId" FROM relevant_orgs)
-          AND "deletedAt" IS NULL
-        GROUP BY "organizationId"
+        SELECT osa."organizationId", sum(osa."memberCount") AS total_count
+        FROM "organizationSegmentsAgg" osa
+        JOIN segments s ON s.id = osa."segmentId"
+          AND s."grandparentId" IS NOT NULL
+          AND s."parentId" IS NOT NULL
+        WHERE osa."organizationId" IN (SELECT "organizationId" FROM relevant_orgs)
+        GROUP BY osa."organizationId"
       )
       SELECT
         mo.id,
