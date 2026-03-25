82a1b56132a3d71245d363c140ecd4a24b5e605e
Umberto Sgueglia <usgueglia@contractor.linuxfoundation.org>
2026-03-25 12:18:34 +0100

    fix: revert not found inside the routher
    Signed-off-by: Umberto Sgueglia <usgueglia@contractor.linuxfoundation.org>

--- backend/src/api/public/index.ts
+++ backend/src/api/public/index.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express'
 
-import { NotFoundError } from '@crowd/common'
-
 import { errorHandler } from './middlewares/errorHandler'
 import { v1Router } from './v1'
 
@@ -9,11 +7,6 @@ export function publicRouter(): Router {
   const router = Router()
 
   router.use('/v1', v1Router())
-
-  router.use(() => {
-    throw new NotFoundError()
-  })
-
   router.use(errorHandler)
 
   return router
