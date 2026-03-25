ab79811b9a8747c48cbba1f5b4572377f266b01d
Umberto Sgueglia <usgueglia@contractor.linuxfoundation.org>
2026-03-25 11:24:47 +0100

    fix: add 404 error in public router
    Signed-off-by: Umberto Sgueglia <usgueglia@contractor.linuxfoundation.org>

--- backend/src/api/public/index.ts
+++ backend/src/api/public/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 
+import { NotFoundError } from '@crowd/common'
+
 import { errorHandler } from './middlewares/errorHandler'
 import { v1Router } from './v1'
 
@@ -7,6 +9,11 @@ export function publicRouter(): Router {
   const router = Router()
 
   router.use('/v1', v1Router())
+
+  router.use(() => {
+    throw new NotFoundError()
+  })
+
   router.use(errorHandler)
 
   return router
