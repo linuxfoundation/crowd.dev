b63f18c4b9b6d489f9afa6b173787a3188309351
Mouad BANI <mouad-mb@outlook.com>
2026-04-15 17:52:42 +0100

    chore: decomission discord ws
    Signed-off-by: Mouad BANI <mouad-mb@outlook.com>

--- services/libs/integrations/src/integrations/discord/index.ts
+++ services/libs/integrations/src/integrations/discord/index.ts
@@ -6,15 +6,12 @@ import generateStreams from './generateStreams'
 import { DISCORD_MEMBER_ATTRIBUTES } from './memberAttributes'
 import processData from './processData'
 import processStream from './processStream'
-import processWebhookStream from './processWebhookStream'
-
 const descriptor: IIntegrationDescriptor = {
   type: PlatformType.DISCORD,
   memberAttributes: DISCORD_MEMBER_ATTRIBUTES,
   generateStreams,
   processStream,
   processData,
-  processWebhookStream,
 }
 
 export default descriptor
