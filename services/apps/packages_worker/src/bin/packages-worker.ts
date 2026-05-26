cb699848fdc9a4f0d1e32998bb5756601067ec33
anilb <epipav@gmail.com>
2026-05-26 17:36:25 +0200

    feat: add npm-registry-ingest daily schedule
    Signed-off-by: anilb <epipav@gmail.com>

--- services/apps/packages_worker/src/bin/packages-worker.ts
+++ services/apps/packages_worker/src/bin/packages-worker.ts
@@ -1,6 +1,8 @@
+import { scheduleNpmIngest } from '../npm/schedule'
 import { svc } from '../service'
 
 setImmediate(async () => {
   await svc.init()
+  await scheduleNpmIngest()
   await svc.start()
 })
---
+++ services/apps/packages_worker/src/npm/schedule.ts
@@ -0,0 +1,41 @@
+import { ScheduleAlreadyRunning, ScheduleOverlapPolicy } from '@temporalio/client'
+
+import { svc } from '../service'
+
+import { npmHello } from './workflows'
+
+export async function scheduleNpmIngest(): Promise<void> {
+  const { temporal } = svc
+  if (!temporal) throw new Error('Temporal client not initialized')
+
+  try {
+    await temporal.schedule.create({
+      scheduleId: 'npm-registry-ingest',
+      spec: {
+        cronExpressions: ['15 3 * * *'],
+      },
+      policies: {
+        overlap: ScheduleOverlapPolicy.SKIP,
+        catchupWindow: '1 hour',
+      },
+      action: {
+        type: 'startWorkflow',
+        workflowType: npmHello,
+        taskQueue: 'packages-worker',
+        workflowExecutionTimeout: '1 hour',
+        retry: {
+          initialInterval: '30 seconds',
+          backoffCoefficient: 2,
+          maximumAttempts: 3,
+        },
+        args: [],
+      },
+    })
+  } catch (err) {
+    if (err instanceof ScheduleAlreadyRunning) {
+      svc.log.info('Schedule npm-registry-ingest already registered.')
+    } else {
+      throw err
+    }
+  }
+}
