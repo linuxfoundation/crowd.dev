21388b889857013def0458ece156fca8085c4bbb
anilb <epipav@gmail.com>
2026-05-26 17:34:46 +0200

    feat: add npm hello workflow and activity
    Signed-off-by: anilb <epipav@gmail.com>

--- services/apps/packages_worker/src/bin/packages-worker.ts
+++ services/apps/packages_worker/src/bin/packages-worker.ts
@@ -1,53 +1,6 @@
-import fs from 'fs'
-import path from 'path'
+import { svc } from '../service'
 
-import { getServiceLogger } from '@crowd/logging'
-
-import { getPackagesDb } from '../db'
-
-const log = getServiceLogger()
-
-const liveFilePath = path.join(__dirname, '../tmp/packages-worker-live.tmp')
-const readyFilePath = path.join(__dirname, '../tmp/packages-worker-ready.tmp')
-
-let shuttingDown = false
-
-const shutdown = async () => {
-  if (shuttingDown) return
-  shuttingDown = true
-  log.info('Shutting down packages-worker...')
-  process.exit(0)
-}
-
-process.on('SIGINT', shutdown)
-process.on('SIGTERM', shutdown)
-
-const main = async () => {
-  log.info('packages-worker starting...')
-
-  const qx = await getPackagesDb()
-  await qx.selectOne('SELECT 1')
-  log.info('Connected to packages-db.')
-
-  // Create tmp directory for health probe files
-  fs.mkdirSync(path.dirname(liveFilePath), { recursive: true })
-
-  setInterval(async () => {
-    if (shuttingDown) return
-    try {
-      await Promise.all([
-        fs.promises.open(liveFilePath, 'a').then((f) => f.close()),
-        fs.promises.open(readyFilePath, 'a').then((f) => f.close()),
-      ])
-    } catch (err) {
-      log.warn({ err }, 'Failed to write health probe files')
-    }
-  }, 5000)
-
-  log.info('packages-worker started, idle.')
-}
-
-main().catch((err) => {
-  log.error({ err }, 'packages-worker fatal error')
-  process.exit(1)
+setImmediate(async () => {
+  await svc.init()
+  await svc.start()
 })
