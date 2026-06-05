1feb44dff38a26485ef231b81709384c226af5de
Uroš Marolt <uros@marolt.me>
2026-06-05 18:03:48 +0200

    fix: comments
    Signed-off-by: Uroš Marolt <uros@marolt.me>

--- services/apps/packages_worker/src/deps-dev/queries/advisoriesSql.ts
+++ services/apps/packages_worker/src/deps-dev/queries/advisoriesSql.ts
@@ -34,5 +34,6 @@ FROM \`bigquery-public-data.deps_dev_v1.AdvisoriesLatest\` a,
 UNNEST(a.Packages) AS pkg
 LEFT JOIN purl_map pm ON pm.System = pkg.System AND pm.Name = pkg.Name
 WHERE pkg.System IN (${systems})
+  AND pkg.Name NOT LIKE '%>%'
 `
 }
