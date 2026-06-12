efce1f8c223e151b2901ccf2cf98d3d43bdd105c
Uroš Marolt <uros@marolt.me>
2026-06-12 08:17:09 +0200

    fix: comments
    Signed-off-by: Uroš Marolt <uros@marolt.me>

--- services/apps/packages_worker/src/scorecard/queries/scorecardSql.ts
+++ services/apps/packages_worker/src/scorecard/queries/scorecardSql.ts
@@ -2,7 +2,10 @@ import { SCORECARD_DATASET } from '../../deps-dev/config'
 
 export const SCORECARD_REPOS_SQL = `
 SELECT
-  LOWER(CONCAT('https://', repo.name)) AS repo_url,
+  CASE
+    WHEN repo.name LIKE 'github.com/%' THEN LOWER(CONCAT('https://', repo.name))
+    ELSE CONCAT('https://', repo.name)
+  END AS repo_url,
   score,
   date AS scanned_at
 FROM \`${SCORECARD_DATASET}.scorecard-v2_latest\`
@@ -11,7 +14,10 @@ WHERE repo.name IS NOT NULL
 
 export const SCORECARD_CHECKS_SQL = `
 SELECT
-  LOWER(CONCAT('https://', r.repo.name)) AS repo_url,
+  CASE
+    WHEN r.repo.name LIKE 'github.com/%' THEN LOWER(CONCAT('https://', r.repo.name))
+    ELSE CONCAT('https://', r.repo.name)
+  END AS repo_url,
   c.name   AS check_name,
   c.score  AS check_score,
   c.reason AS check_reason
